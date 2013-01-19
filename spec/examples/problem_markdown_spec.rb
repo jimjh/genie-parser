af4b7d9daa50ee79c557d145e1d51132f99fc66e
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-01-19 12:07:55 -0500

    enabled markdown in question strings

--- spec/examples/problem_markdown_spec.rb
+++ spec/examples/problem_markdown_spec.rb
@@ -3,9 +3,17 @@ require 'spec_helper'
 
 describe Spirit::Render::Problem do
 
+  def render(yaml)
+    Spirit::Render::Problem.parse(yaml).render(index: 0)
+  end
+
   describe '::parse' do
 
-    it 'allows markdown in the question text'
+    it 'allows markdown in the question text' do
+      yaml = FactoryGirl.create(:short, question: '*hi*')
+      exp  = '<em>hi</em>'
+      render(yaml).should match(/#{exp}/)
+    end
 
   end
 
