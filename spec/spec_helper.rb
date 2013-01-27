84ea31575942982b2989a9282e37752fb82331e9
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-01-27 18:02:57 -0500

    configured rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,3 +21,9 @@ end
 $:.unshift Test::ROOT + '..' + 'lib'
 require 'spirit'
 FactoryGirl.find_definitions
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+end
